25e4a96c0c95e7c676ae73a7101c1e6fdefce1c9
Venkat <venkata@venkatamutyala.com>
2025-01-04 08:04:17 +0000

    feat: add new endpoints so that we can dispatch nukes to the gha-aws-cleanup actions and rename existing endpoints

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -6,17 +6,21 @@ class Message(BaseModel):
 
 
 class CreateLightsailRequest(BaseModel):
-    captain_domain: str
-    region: str
+    captain_domain: str  = Field(...,example = 'glueops-captain-foobar')
+    region: str = Field(...,example = 'us-west-2')
 
 class DeleteLightsailRequest(BaseModel):
-    captain_domain: str
-    region: str
-
+    captain_domain: str  = Field(...,example = 'nonprod.foobar.onglueops.rocks')
+    region: str = Field(...,example = 'us-west-2')
 
 class StorageBucketsRequest(BaseModel):
-    captain_domain: str
+    captain_domain: str = Field(...,example = 'nonprod.foobar.onglueops.rocks')
 
 class AwsCredentialsRequest(BaseModel):
-    aws_sub_account_name: str
+    aws_sub_account_name: str = Field(...,example = 'glueops-captain-foobar')
 
+class AwsNukeAccountRequest(BaseModel):
+    aws_sub_account_name: str  = Field(...,example = 'glueops-captain-foobar')
+
+class CaptainDomainNukeDataAndBackupsRequest(BaseModel):
+    captain_domain: str = Field(...,example = 'nonprod.foobar.onglueops.rocks')
